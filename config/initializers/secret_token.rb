cb571a99f9f016e413958d06268487fb4152c9ee
Olivier Sallou <olivier.sallou@debian.org>
2012-10-09 16:56:20 +0200

    first import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Training::Application.config.secret_token = '66c0a3f20b99c657f9345aeb9b13ae1cfe6bd8718b5c94a87de515dc214015fe48dec787097ae8216a18e15b792cff7785cff8878e49c8ad17e34e957c16b538'
