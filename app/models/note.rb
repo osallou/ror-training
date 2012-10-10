0a696f384b03e149ace0d4ff29255e321a7cfa12
Olivier Sallou <olivier.sallou@debian.org>
2012-10-10 12:12:56 +0200

    add unit tests

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,7 @@
 class Note < ActiveRecord::Base
   attr_accessible :content, :user
+
+  def self.search(name)
+    notes = Note.all(:conditions => ['user LIKE ?', "%#{name}%"])
+  end
 end
--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -4,4 +4,15 @@ class NoteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "find notes" do
+    found = Note.where(:user => 'test').all.count
+    assert found>0
+  end
+
+  test "search notes" do
+    notes = Note.search('ll')
+    puts notes.inspect
+    assert notes.count > 0
+  end
 end
