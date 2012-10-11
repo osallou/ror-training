83b0dbc8c28dbc99f0e07bc211c3b9d7e5ec0c43
Olivier Sallou <olivier.sallou@debian.org>
2012-10-11 09:52:01 +0200

    add validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,9 @@
 class Note < ActiveRecord::Base
   attr_accessible :content, :title
 
+  validates_presence_of :title
+  validates_length_of :title, :minimum => 10, :too_short => "please enter a singnificative title (>=10 characters)"
+
   def self.search(name)
     notes = Note.all(:conditions => ['lower(title) LIKE ? OR lower(content) LIKE ?', "%#{name}%", "%#{name}%"])
   end
